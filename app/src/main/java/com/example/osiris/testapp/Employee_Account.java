eb837a74d95b50ec455e90aac5f9d5b56a6c92ab
DUMNEZO <r_stoica21@yahoo.ro>
2017-11-15 15:04:27 +0100

    employee_account changes

--- app/src/main/java/com/example/osiris/testapp/Employee_Account.java
+++ app/src/main/java/com/example/osiris/testapp/Employee_Account.java
@@ -5,18 +5,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Calendar;
 
 public class Employee_Account extends AppCompatActivity {
 
     private Button button;
     private FirebaseAuth mAuth;
 
+    private Calendar calendar = Calendar.getInstance();
+
+    private EditText day, month, year;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account__employee);
+
+        day = (EditText) findViewById(R.id.day);
+        month = (EditText) findViewById(R.id.month);
+        year = (EditText) findViewById(R.id.year);
+        int day2 = calendar.get(Calendar.DAY_OF_MONTH);
+        int month2 = calendar.get(Calendar.MONTH);
+        int year2 = calendar.get(Calendar.YEAR);
+
+        day.setText(String.valueOf(day2));
+        month.setText(String.valueOf(month2));
+        year.setText(String.valueOf(year2));
 
         mAuth = FirebaseAuth.getInstance();
         button = (Button)findViewById(R.id.logoutButtonEmployee);
