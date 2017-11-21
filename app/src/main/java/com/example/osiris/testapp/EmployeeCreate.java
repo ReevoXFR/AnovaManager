9f6d7f9b76cb3dc64817c4bfcc7d0a744accdc8c
ReevoXFR <reevotrailers@gmail.com>
2017-11-21 11:44:41 +0100

    Finisari v2.0

--- app/src/main/java/com/example/osiris/testapp/EmployeeCreate.java
+++ app/src/main/java/com/example/osiris/testapp/EmployeeCreate.java
@@ -1,13 +1,11 @@
 package com.example.osiris.testapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -30,7 +28,7 @@ public class EmployeeCreate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_employee_create);
+        setContentView(R.layout.activity_employee_add);
         mAuth = FirebaseAuth.getInstance();
 
 
